615cfb7063098efeb264d15fcd1354605a4e9fd4
Mukund Singh Kathait <mukundsingh@usf.edu>
2020-01-21 20:57:13 -0500

    Add project files.

---
+++ ExcerciseTwo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ExcerciseTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 25;
+            int height = 175;
+            //Max heart rate = 220 - age
+            Console.WriteLine("I am " + age + " years old, my max heart rate is " + (220 - age) +
+                " and I am "+ height+  " centimeters tall");
+
+
+        }
+    }
+}
